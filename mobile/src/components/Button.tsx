7b5d9362f4f15d6dccfd369683dc3a5e2af7a20c
mgckaled <marcelgckaled@gmail.com>
2023-01-25 20:08:11 -0300

    feat: create Button component and adjust screen styles

---
+++ mobile/src/components/Button.tsx
@@ -0,0 +1,39 @@
+import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base'
+
+type Props = IButtonProps & {
+	title: string
+	bgColor: 'blue.500' | 'gray.700' | 'gray.300'
+	_borderColor: 'blue.700' | 'black' | 'gray.400'
+	textColor: 'gray.100' | 'gray.600'
+	widthSize?: 'full' | '45%'
+}
+
+export function Button({
+	title,
+	bgColor,
+	_borderColor,
+	textColor,
+	widthSize = 'full',
+	...rest
+}: Props) {
+	return (
+		<ButtonNativeBase
+			w={widthSize}
+			h={12}
+			bg={bgColor}
+			borderWidth={1}
+			borderColor={_borderColor}
+			rounded="md"
+			justifyItems="center"
+			_pressed={{
+				opacity: 0.9,
+				backgroundColor: bgColor
+			}}
+			{...rest}
+		>
+			<Text color={textColor} fontFamily="heading" fontSize="sm">
+				{title}
+			</Text>
+		</ButtonNativeBase>
+	)
+}
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,34 +1,56 @@
 import { Center, Image, Stack, Text, VStack } from 'native-base'
 
 import logoImgPng from '@assets/logo.png'
+import { Button } from '@components/Button'
 
 export function SignIn() {
 	return (
 		<Stack flex={1} safeArea>
-			<VStack flex={1} bg="gray.100" justifyContent="center" roundedBottom={32}>
+			<Stack bg="gray.100" roundedBottom={32}>
+				<VStack mx={10} mt={12} justifyContent="center">
+					<Center>
+						<Image
+							source={logoImgPng}
+							alt="logo do app"
+							resizeMode="contain"
+							size={32}
+						/>
+						<Text fontFamily="heading" fontSize="2xl">
+							marketspace
+						</Text>
+						<Text fontFamily="body" fontSize="sm" color="gray.400">
+							seu espaço de compra e venda
+						</Text>
+						<Text
+							mt={12}
+							mb={3}
+							fontFamily="body"
+							fontSize="sm"
+							color="gray.500"
+						>
+							Acesse sua conta
+						</Text>
+						<Button
+							title="Entrar"
+							bgColor="blue.500"
+							_borderColor="blue.700"
+							textColor="gray.100"
+						/>
+					</Center>
+				</VStack>
+			</Stack>
+			<VStack mx={10} mt={12} justifyContent="center">
 				<Center>
-					<Image
-						source={logoImgPng}
-						alt="logo do app"
-						resizeMode="contain"
-						size={32}
-					/>
-					<Text fontFamily="heading" fontSize="2xl">
-						marketspace
-					</Text>
-					<Text fontFamily="body" fontSize="sm" color="gray.400">
-						seu espaço de compra e venda
-					</Text>
-					<Text mt={12} fontFamily="body" fontSize="sm" color="gray.500">
-						Acesse sua conta
-					</Text>
-				</Center>
-			</VStack>
-			<VStack>
-				<Center>
-					<Text mt={16} fontFamily="body" fontSize="sm" color="gray.500">
+					<Text fontFamily="body" fontSize="sm" color="gray.500">
 						Ainda não tem acesso?
 					</Text>
+					<Button
+						mt={4}
+						title="Criar uma conta"
+						bgColor="gray.300"
+						_borderColor="gray.400"
+						textColor="gray.600"
+					/>
 				</Center>
 			</VStack>
 		</Stack>
