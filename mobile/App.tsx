8c0d66d6c3540bc5952a18a22535962afe371a80
mgckaled <marcelgckaled@gmail.com>
2023-01-24 21:09:04 -0300

    feat: install native-base dependencies and add its provider do App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,13 +3,16 @@ import {
 	Karla_700Bold,
 	useFonts
 } from '@expo-google-fonts/karla'
+import { NativeBaseProvider } from 'native-base'
 import { Text, View } from 'react-native'
 
 export default function App() {
 	const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
 	return (
-		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-			{fontsLoaded ? <Text>Hello World</Text> : <View />}
-		</View>
+		<NativeBaseProvider>
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				{fontsLoaded ? <Text>Hello World</Text> : <View />}
+			</View>
+		</NativeBaseProvider>
 	)
 }
