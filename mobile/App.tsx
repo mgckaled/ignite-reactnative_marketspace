0158dc4db02ca91434961c65ab4f2b175c62e893
mgckaled <marcelgckaled@gmail.com>
2023-01-24 21:27:07 -0300

    feat: add StatusBar to App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import {
 	useFonts
 } from '@expo-google-fonts/karla'
 import { NativeBaseProvider } from 'native-base'
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 
 import { THEME } from './src/theme'
 
@@ -12,7 +12,14 @@ export default function App() {
 	const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
 	return (
 		<NativeBaseProvider theme={THEME}>
-			<View>{fontsLoaded ? <Text>Hello World</Text> : <View />}</View>
+			<View>
+				<StatusBar
+					barStyle="dark-content"
+					backgroundColor="transparent"
+					translucent
+				/>
+				{fontsLoaded ? <Text>Hello World</Text> : <View />}
+			</View>
 		</NativeBaseProvider>
 	)
 }
