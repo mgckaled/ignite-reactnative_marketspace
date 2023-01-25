c2a28acb46f6b68336a9346dd616589496eb34bf
mgckaled <marcelgckaled@gmail.com>
2023-01-24 21:25:24 -0300

    feat: add custom global styles

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,13 +6,13 @@ import {
 import { NativeBaseProvider } from 'native-base'
 import { Text, View } from 'react-native'
 
+import { THEME } from './src/theme'
+
 export default function App() {
 	const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
 	return (
-		<NativeBaseProvider>
-			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-				{fontsLoaded ? <Text>Hello World</Text> : <View />}
-			</View>
+		<NativeBaseProvider theme={THEME}>
+			<View>{fontsLoaded ? <Text>Hello World</Text> : <View />}</View>
 		</NativeBaseProvider>
 	)
 }
