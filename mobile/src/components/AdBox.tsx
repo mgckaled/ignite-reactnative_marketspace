6d3c313c40192eed345637cadff1f2a46d2c54ba
mgckaled <marcelgckaled@gmail.com>
2023-01-27 14:38:12 -0300

    feat: create adBox component and add it to Home screen

---
+++ mobile/src/components/AdBox.tsx
@@ -0,0 +1,44 @@
+import {
+	HStack,
+	Heading,
+	IPressableProps,
+	Pressable,
+	Text,
+	VStack,
+	useTheme
+} from 'native-base'
+import { ArrowRight, Tag } from 'phosphor-react-native'
+
+export function AdBox({ ...rest }: IPressableProps) {
+	const { colors } = useTheme()
+
+	return (
+		<Pressable
+			h={20}
+			mt={3}
+			bg="coolGray.200"
+			rounded="lg"
+			borderWidth={1}
+			borderColor="coolGray.300"
+			alignItems="center"
+			_pressed={{ opacity: 0.8 }}
+			{...rest}
+		>
+			<HStack flex={1} justifyContent="space-between" alignItems="center">
+				<Tag size={24} color={colors.blue[500]} />
+				<VStack mx={4}>
+					<Heading fontFamily="heading" fontSize="lg">
+						4
+					</Heading>
+					<Text fontFamily="body" fontSize="xs">
+						anúncios ativos
+					</Text>
+				</VStack>
+				<Text mx={3} fontFamily="body" fontSize="sm" color="blue.700">
+					Meus anúncios
+				</Text>
+				<ArrowRight size={24} color={colors.blue[500]} />
+			</HStack>
+		</Pressable>
+	)
+}
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,12 +1,30 @@
-import { Stack, VStack } from 'native-base'
+import { useNavigation } from '@react-navigation/native'
+import { Stack, Text, VStack } from 'native-base'
 
+import { AdBox } from '@components/AdBox'
 import { HomeHeader } from '@components/HomeHeader'
 
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
 export function Home() {
+	// navigation
+	const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+	function handleOpenAd() {
+		navigation.navigate('ad')
+	}
+
 	return (
 		<Stack flex={1} bg="gray.200" safeArea>
-			<VStack mx={6}>
+			<VStack mx={8}>
 				<HomeHeader />
+				<Text ml={1} mt={3} fontFamily="body" fontSize="sm" color="gray.400">
+					Seus produtos anunciados para venda
+				</Text>
+				<AdBox onPress={handleOpenAd} />
+				<Text ml={1} mt={7} fontFamily="body" fontSize="sm" color="gray.400">
+					Compre produtos variados
+				</Text>
 			</VStack>
 		</Stack>
 	)
