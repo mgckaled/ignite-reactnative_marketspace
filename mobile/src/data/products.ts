5b77f927ae7c13a3598a3ebd6704232fc4443890
mgckaled <marcelgckaled@gmail.com>
2023-02-02 00:20:02 -0300

    feat: update main project data

--- mobile/src/data/products.ts
+++ mobile/src/data/products.ts
@@ -1,25 +1,28 @@
 export const PRODUCTS = [
 	{
 		id: '1',
-		name: 'Tênis Vermelho',
-		price: '59,90',
-		thumb: require('../assets/products/tenis-vermelho_thumb.png'),
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti sapiente corrupti rerum, blanditiis illum veritatis! Dignissimos nihil incidunt sed ipsam provident, eaque minus sunt sequi voluptates veritatis iste vel doloremque.'
-	},
-	{
-		id: '2',
 		name: 'Bicicleta',
 		price: '120,00',
 		thumb: require('../assets/products/bicicleta_thumb.png'),
+		image: require('../assets/products/bicicleta.png'),
 		description:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugiat nam consequuntur, optio odio expedita ad ipsa voluptas. Provident quos itaque praesentium doloribus dolores aut aperiam sit ipsa ducimus rerum.'
+	},
+	{
+		id: '2',
+		name: 'Tênis Vermelho',
+		price: '59,90',
+		thumb: require('../assets/products/tenis-vermelho_thumb.png'),
+		image: require('../assets/products/bicicleta.png'),
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti sapiente corrupti rerum, blanditiis illum veritatis! Dignissimos nihil incidunt sed ipsam provident, eaque minus sunt sequi voluptates veritatis iste vel doloremque.'
 	},
 	{
 		id: '3',
 		name: 'Mesa de cabeceira',
 		price: '59,90',
 		thumb: require('../assets/products/mesa-cabeceira_thumb.png'),
+		image: require('../assets/products/bicicleta.png'),
 		description:
 			'Provident quos itaque praesentium doloribus dolores aut aperiam sit ipsa ducimus rerum.'
 	},
@@ -28,6 +31,7 @@ export const PRODUCTS = [
 		name: 'Sofá 1,80m',
 		price: '199,00',
 		thumb: require('../assets/products/sofa_thumb.png'),
+		image: require('../assets/products/bicicleta.png'),
 		description:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugiat nam consequuntur, optio odio expedita ad ipsa voluptas.'
 	},
@@ -36,6 +40,7 @@ export const PRODUCTS = [
 		name: 'Botas',
 		price: '99,90',
 		thumb: require('../assets/products/bota_thumb.png'),
+		image: require('../assets/products/bicicleta.png'),
 		description:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugiat nam consequuntur, optio odio expedita ad ipsa voluptas. Provident quos itaque praesentium doloribus dolores aut aperiam sit ipsa ducimus rerum.'
 	},
@@ -44,6 +49,7 @@ export const PRODUCTS = [
 		name: 'Camisa',
 		price: '67,00',
 		thumb: require('../assets/products/camisa_thumb.png'),
+		image: require('../assets/products/bicicleta.png'),
 		description:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugiat nam consequuntur, optio odio expedita ad ipsa voluptas. Provident quos itaque praesentium doloribus dolores aut aperiam sit ipsa ducimus rerum.'
 	},
@@ -52,6 +58,7 @@ export const PRODUCTS = [
 		name: 'Luminária',
 		price: '109,90',
 		thumb: require('../assets/products/luminaria_thumb.png'),
+		image: require('../assets/products/bicicleta.png'),
 		description:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugiat nam consequuntur, optio odio expedita ad ipsa voluptas.'
 	}
