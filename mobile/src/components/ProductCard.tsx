9b010278acd7bd8c3da9d60aa28c1be646ea5129
mgckaled <marcelgckaled@gmail.com>
2023-02-02 00:18:22 -0300

    feat: update Products and ProductCard to properly insert data

--- mobile/src/components/ProductCard.tsx
+++ mobile/src/components/ProductCard.tsx
@@ -1,49 +1,25 @@
-import { useNavigation } from '@react-navigation/native'
 import { Heading, Image, Text, VStack } from 'native-base'
-import {
-	ImageSourcePropType,
-	TouchableOpacity,
-	TouchableOpacityProps
-} from 'react-native'
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
 
-import { examplePNG } from '@assets/products/bota_thumb.png'
-
-import { AppNavigatorRoutesProps } from '@routes/app.routes'
-
-export type ProductCardProps = {
-	id: string
-	name: string
-	description: string
-	price: string
-	thumb: ImageSourcePropType
-}
+import { ProductDTO } from '@dtos/ProductDTO'
 
 type Props = TouchableOpacityProps & {
-	data?: ProductCardProps
+	data: ProductDTO
 }
 
-export function ProductCard({ ...rest }: Props) {
-	const { navigate } = useNavigation<AppNavigatorRoutesProps>()
-
+export function ProductCard({ data, ...rest }: Props) {
 	return (
-		<TouchableOpacity onPress={() => navigate('details')} {...rest}>
+		<TouchableOpacity {...rest}>
 			<VStack w={40} h={24} rounded="lg" bg="gray.300" m={1} p={2}>
-				<Image
-					source={examplePNG}
-					defaultSource={examplePNG}
-					alt="Imagem do produto"
-					resizeMode="cover"
-				/>
+				<Image source={data.thumb} alt="Imagem do produto" resizeMode="cover" />
 			</VStack>
 
 			<VStack>
 				<Text ml={2} color="gray.400" fontSize="sm">
-					{/* {data.name} */}
-					Sofa 1.80m
+					{data.name}
 				</Text>
 				<Heading color="gray.700" fontFamily="heading" fontSize="md" ml={2}>
-					{/* R$ {data.price} */}
-					R$ 99,90
+					R$ {data.price}
 				</Heading>
 			</VStack>
 		</TouchableOpacity>
--- mobile/src/components/Products.tsx
+++ mobile/src/components/Products.tsx
@@ -1,18 +1,28 @@
+import { useNavigation } from '@react-navigation/native'
 import { FlatList, VStack } from 'native-base'
 
-import { ProductCard, ProductCardProps } from './ProductCard'
+import { ProductDTO } from '@dtos/ProductDTO'
+import { ProductCard } from './ProductCard'
 
 type Props = {
-	data: ProductCardProps[]
+	data: ProductDTO[]
 }
 
 export function Products({ data }: Props) {
+	// navigation
+	const { navigate } = useNavigation()
+
 	return (
 		<VStack>
 			<FlatList
 				data={data}
 				keyExtractor={item => item.id}
-				renderItem={({ item }) => <ProductCard data={item} />}
+				renderItem={({ item }) => (
+					<ProductCard
+						data={item}
+						onPress={() => navigate('details', { productId: item.id })}
+					/>
+				)}
 				numColumns={2}
 				_contentContainerStyle={{ alignItems: 'center', paddingBottom: 20 }}
 				showsVerticalScrollIndicator={false}
